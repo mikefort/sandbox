6ef95fb557a352c92184a66090704134c2ff65b8
danlindow <dan.lindow@gmail.com>
2014-12-05 01:22:13 -0800

    fix ASCII out of range errors

--- reddit-new-posts.py
+++ reddit-new-posts.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 #imports
 import praw
 import pprint
@@ -6,9 +7,12 @@ import time
 #setup PRAW
 user_agent = ("test API please ignore")
 r = praw.Reddit(user_agent=user_agent)
-submissions = r.get_subreddit('all').get_new(limit=5)
-
+submissions = r.get_subreddit('all').get_new(limit=10)
+#print all the things
 for x in submissions:
 	title = x.title
 	subreddit = x.subreddit.display_name
+	#make friendly
+	title = title.encode('ascii', 'ignore')
+	subreddit = subreddit.encode('ascii', 'ignore')
 	print "%s - %s" % (subreddit, title)
