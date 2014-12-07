0c87a55a5adfec4800b79042bdc05e5f539946d4
danlindow <dan.lindow@gmail.com>
2014-12-07 01:46:27 -0800

    updates

--- reddit-new-posts.py
+++ reddit-new-posts.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 #imports
 import praw
-import pprint
 import time
+import pprint
 
 #setup PRAW
 user_agent = ("test API please ignore")
 r = praw.Reddit(user_agent=user_agent)
-submissions = r.get_subreddit('all').get_new(limit=10)
+posts = r.get_subreddit('all').get_hot(limit=10)
 #print all the things
-for x in submissions:
+for x in posts:
 	title = x.title
 	subreddit = x.subreddit.display_name
+	url = x.url
 	#make friendly
 	title = title.encode('ascii', 'ignore')
 	subreddit = subreddit.encode('ascii', 'ignore')
-	print "%s - %s" % (subreddit, title)
+	url = url.encode('ascii', 'ignore')
+	print "%s - %s : %s" % (subreddit, title, url)
